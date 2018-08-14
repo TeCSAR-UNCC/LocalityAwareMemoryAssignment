75ff7ca50f794a491eb7575e0e73b213e1e79b48
Samuel Rogers <sroger48@uncc.edu>
2018-08-14 13:24:52 -0400

    Uploaded Project

---
+++ runTests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./src/llvm_parser test/aes.ll
+./src/llvm_parser test/bfs.ll
+./src/llvm_parser test/gemm.ll
+./src/llvm_parser test/gemm-unrolled.ll
+./src/llvm_parser test/md.ll
+./src/llvm_parser test/md-grid.ll
+./src/llvm_parser test/spmv.ll
+./src/llvm_parser test/stencil2d.ll
+./src/llvm_parser test/stencil3d.ll
+
